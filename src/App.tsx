556a914b92abc796d0d7ea8010b2302cbf492f06
ecarnovsky <ecarnovsky@gmail.com>
2024-07-01 17:46:43 -0400

    Added signup and login pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import {
   BrowserRouter as Router,
   Routes, Route, Link
 } from 'react-router-dom'
-import Home from './pages/index'
+import Home from './pages/home'
 import About from './pages/about'
 import CreateNote from './pages/create-note'
 import MyNotes from './pages/my-notes'
+import Signup from './pages/signup'
+import Login from './pages/login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +20,8 @@ function App() {
         <Route path="/mynotes" element={<MyNotes />} />
         <Route path="/createnote" element={<CreateNote />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/pages/signup.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Signup(){
+    // const [count, setCount] = useState(0)
+    return(
+        <>
+            <h1>Signup Page</h1>
+        </>
+    )
+}
+
+export default Signup
