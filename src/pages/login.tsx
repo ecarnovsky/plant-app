8d5bacfc02ea32bd712e600b8d8eac63b84dfb2e
ecarnovsky <ecarnovsky@gmail.com>
2024-07-29 20:23:10 -0400

    Added the auth form to the login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
 import Layout from '../components/layout'
+import AuthForm from '../supabase-auth'
 
 function Login(){
-    // const [count, setCount] = useState(0)
     return(
         <Layout>
             <h1>Login</h1>
-            <form action="/login" method="POST">
-                <input type="email" name="email" placeholder="Email"></input>
-                <input type="password" name="password" placeholder="Password"></input>
-                <input type="submit" value="Log in"></input>
-            </form>
+            <AuthForm/>
         </Layout>
     )
 }
